74541ced9d28c0cbb06a51a6ee947e9678e07119
araskara <arazgare1@gmail.com>
2023-08-24 14:30:56 +0200

    Update

--- lib/profile.dart
+++ lib/profile.dart
@@ -16,15 +16,24 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   Future<Map<String, dynamic>>? _userActivityFuture;
   final authManager = AuthManager();
+  int userScore = 0;
 
   @override
   void initState() {
     super.initState();
     _userActivityFuture = fetchUserActivity();
+
+    fetchUserScore().then((score) {
+      setState(() {
+        userScore = score;
+      });
+    }).catchError((error) {
+      // Handle any errors here if needed
+    });
   }
 
   Future<Map<String, dynamic>> fetchUserActivity() async {
-    final authToken = authManager.authToken; // Get token from your AuthManager
+    final authToken = authManager.authToken;
 
     final response = await http.get(
       Uri.parse('https://wk.up.railway.app/polls/user_activity/'),
@@ -57,6 +66,110 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
     }
   }
 
+  Future<void> updatePoll(
+      int pollId, String correctAnswer, String proofLink) async {
+    final authToken = authManager.authToken;
+
+    final response = await http.put(
+      Uri.parse('https://wk.up.railway.app/polls/$pollId/'),
+      headers: {
+        'Authorization': 'Token $authToken',
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'correct_answer': correctAnswer,
+        'proof_link': proofLink,
+      }),
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to update the poll');
+    }
+  }
+
+  Future<int> fetchUserScore() async {
+    final authToken = authManager.authToken;
+
+    final response = await http.get(
+      Uri.parse('https://wk.up.railway.app/user/score/'),
+      headers: {
+        'Authorization': 'Token $authToken',
+        'Content-Type': 'application/json',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      int score = json.decode(response.body)['score'] ?? 0;
+      return score;
+    } else {
+      throw Exception('Failed to load user score');
+    }
+  }
+
+  void showUpdateDialog(int pollId) {
+    String correctAnswer = 'yes'; // default value set to 'yes'
+    String proofLink = '';
+
+    showDialog<void>(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Update Poll'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                DropdownButtonFormField<String>(
+                  items: ["yes", "no"].map((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (value) {
+                    correctAnswer = value!;
+                  },
+                  decoration: InputDecoration(labelText: 'Correct Answer'),
+                  value: correctAnswer,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Proof Link'),
+                  onChanged: (value) {
+                    proofLink = value;
+                  },
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Update'),
+              onPressed: () async {
+                try {
+                  await updatePoll(pollId, correctAnswer, proofLink);
+                  setState(() {
+                    _userActivityFuture = fetchUserActivity();
+                  });
+                  Navigator.of(context).pop();
+                } catch (error) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Failed to update poll')),
+                  );
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -70,6 +183,12 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
               Tab(text: 'My Votes'),
             ],
           ),
+          actions: [
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15.0),
+              child: Center(child: Text('Score: $userScore')),
+            ),
+          ],
         ),
         body: FutureBuilder<Map<String, dynamic>>(
           future: _userActivityFuture,
@@ -81,53 +200,57 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             } else if (snapshot.hasData) {
               return TabBarView(
                 children: [
-                  // My Polls
                   ListView(
-                    children: [
-                      ...snapshot.data!['polls']
-                          .map((poll) => ListTile(
-                                title: Text(poll['question']),
-                                trailing: IconButton(
-                                  icon: Icon(Icons.delete),
-                                  onPressed: () async {
-                                    try {
-                                      await deletePoll(poll['id']);
-                                      // Refresh the user activity after deletion
-                                      setState(() {
-                                        _userActivityFuture =
-                                            fetchUserActivity();
-                                      });
-                                    } catch (error) {
-                                      ScaffoldMessenger.of(context)
-                                          .showSnackBar(
-                                        SnackBar(
-                                            content:
-                                                Text('Failed to delete poll')),
-                                      );
-                                    }
-                                  },
-                                ),
-                              ))
-                          .toList(), // Convert map to list
-                    ],
+                    children: snapshot.data!['polls'].map<Widget>((poll) {
+                      return ListTile(
+                        title: Text(poll['question']),
+                        subtitle: Text(
+                            'Yes: ${poll['yes_percentage']}%, No: ${poll['no_percentage']}%'),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            IconButton(
+                              icon: Icon(Icons.edit),
+                              onPressed: () {
+                                showUpdateDialog(poll['id']);
+                              },
+                            ),
+                            IconButton(
+                              icon: Icon(Icons.delete),
+                              onPressed: () async {
+                                try {
+                                  await deletePoll(poll['id']);
+                                  setState(() {
+                                    _userActivityFuture = fetchUserActivity();
+                                  });
+                                } catch (error) {
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                        content: Text('Failed to delete poll')),
+                                  );
+                                }
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                    }).toList(),
                   ),
-                  // My Votes
                   ListView(
-                    children: [
-                      ...snapshot.data!['votes']
-                          .map((vote) => ListTile(
-                                title: Text('Vote for Poll ${vote['poll']}'),
-                                subtitle: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    Text('Choice: ${vote['choice']}'),
-                                    Text(
-                                        'Yes: ${vote['yes_percentage']}%, No: ${vote['no_percentage']}%'),
-                                  ],
-                                ),
-                              ))
-                          .toList(), // Convert map to list
-                    ],
+                    children: snapshot.data!['votes'].map<Widget>((vote) {
+                      return ListTile(
+                        title:
+                            Text('Vote for Poll: ${vote['poll']['question']}'),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text('Choice: ${vote['choice']}'),
+                            if (vote['proof_link'] != null)
+                              Text('Proof: ${vote['proof_link']}'),
+                          ],
+                        ),
+                      );
+                    }).toList(),
                   ),
                 ],
               );
@@ -135,6 +258,18 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
               return Center(child: Text('No activity found'));
             }
           },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => CreatePoll.CreatePollScreen(),
+              ),
+            );
+          },
+          tooltip: 'Create Poll',
+          child: Icon(Icons.add),
         ),
       ),
     );
